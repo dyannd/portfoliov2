3c3dc5b42bd24556063c195fc47b8e65ec12b1d9
Anh Nguyen <anhdng8@gmail.com>
2023-07-07 10:14:57 -0500

    Initial commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Switch from '../Props/Switch';
+import './Home.scss';
+
+const Home = () => (
+    <div className="home">
+        <Switch />
+    </div>
+)
+
+export default Home;
---
+++ src/Components/Props/Switch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Switch.scss';
+
+const Switch = () => (
+    <div className="switch centering-parent">
+        <div className="switch-button"></div>
+        <p className="switch-text light-text">Light</p>
+        <p className="switch-text dark-text">Dark</p>
+    </div>
+)
+
+export default Switch;
